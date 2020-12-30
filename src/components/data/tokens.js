e6b23b28e9da46c16aeb1f80531d9951bca7bc39
dvasin <dvasin@wavesplatform.com>
2020-12-30 14:53:33 +0300

    Fixed tokens icons

--- src/components/data/tokens.js
+++ src/components/data/tokens.js
@@ -51,19 +51,19 @@ export const currencyInfo = {
     symbol: "USDT",
     address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
     decimals: 6,
-    image: "type2",
+    image: "type4",
   },
   usdc: {
     symbol: "USDC",
     address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     decimals: 6,
-    image: "type4",
+    image: "type5",
   },
   dai: {
     symbol: "DAI",
     address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     decimals: 18,
-    image: "type5",
+    image: "type3",
   },
 };
 

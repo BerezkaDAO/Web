21e7561c11aa85ad9d18670f152ba9f698941904
dvasin <dvasin@wavesplatform.com>
2021-01-08 15:39:55 +0300

    Fixed build script

--- publish-docker-prom.sh
+++ publish-docker-prom.sh
@@ -1,2 +1,2 @@
-docker tag -t rg.fr-par.scw.cloud/easychain/berezka/berezka-dashboard-new:latest cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod
+docker tag rg.fr-par.scw.cloud/easychain/berezka/berezka-dashboard-new:latest cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod
 docker push cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod

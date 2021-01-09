abfe37e479944a5a4188d5c34217f6497443efc7
dvasin <dvasin@wavesplatform.com>
2021-01-09 21:15:50 +0300

    Add one click update

--- publish-docker-prom.sh
+++ publish-docker-prom.sh
@@ -1,2 +1,6 @@
 docker tag rg.fr-par.scw.cloud/easychain/berezka/berezka-dashboard-new:latest cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod
 docker push cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod
+
+ssh toor@178.154.234.156 <<'ENDSSH'
+    sh update.sh
+ENDSSH

420cbccda99b9031d6115b4ff3ed3439dba7cde5
dvasin <dvasin@wavesplatform.com>
2021-01-17 22:23:35 +0300

    Update to new static ip

--- publish-docker-prom.sh
+++ publish-docker-prom.sh
@@ -1,6 +1,6 @@
 docker tag rg.fr-par.scw.cloud/easychain/berezka/berezka-dashboard-new:latest cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod
 docker push cr.yandex/crplb38ladkov5f9q8bv/berezka-ui:prod
 
-ssh toor@178.154.234.156 <<'ENDSSH'
+ssh toor@84.201.151.165 <<'ENDSSH'
     sh update.sh
 ENDSSH

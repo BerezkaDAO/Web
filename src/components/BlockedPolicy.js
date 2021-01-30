da63c1784741ace8ad634ce7eb71dfff29baab70
dvasin <dvasin@wavesplatform.com>
2021-01-30 22:04:38 +0300

    GEO design for mobile

--- src/components/BlockedPolicy.js
+++ src/components/BlockedPolicy.js
@@ -7,22 +7,19 @@ class BlockedPolicy extends React.Component {
         <section className="section" style={{ paddingLeft: "190px" }}>
           <div className="section__header">
             <h1
-              className="title"
+              className="title __warning"
               style={{
-                fontWeight: "normal",
                 marginLeft: "-190px",
-                fontSize: "100px",
               }}
             >
               We apologize
             </h1>
             <div
-              className="section__subtitle"
+              className="subtitle __warning"
               style={{
                 color: "white",
                 fontWeight: "normal",
                 marginLeft: "-185px",
-                fontSize: "38px",
               }}
             >
               but the service is not available for your country

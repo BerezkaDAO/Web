d96b43a1f1cb790ff8688fafcd9d6a261f656674
dvasin <dvasin@wavesplatform.com>
2021-02-05 21:05:36 +0300

    Release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+RELEASE_VERSION=$1
+
+git checkout -b release/$RELEASE_VERSION
+sh build-docker.sh
+if [ $? -eq 0 ]
+then
+  git tag -a v$RELEASE_VERSION -m 'Release v. $RELEASE_VERSION'
+  git push origin v$RELEASE_VERSION
+  git push -u origin release/$RELEASE_VERSION
+  sh publish-docker-prom.sh 
+else
+  echo "Could not build docker" >&2
+fi

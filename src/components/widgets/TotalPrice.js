f5cde000bf609135d1d434109cd4c40c6021ae10
dvasin <dvasin@wavesplatform.com>
2021-03-16 22:02:34 +0300

    Hotfix total price

--- src/components/widgets/TotalPrice.js
+++ src/components/widgets/TotalPrice.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useQuery, gql } from "@apollo/react-hooks";
 import { fetchCommonAll } from "./fetchCommon";
 import { round } from "./round";
+import { mergeByDayID } from "./merger";
 
 const GET_LAST_PRICE = `
 query GetTotalPrice($tokens: [String]) {
@@ -24,15 +25,21 @@ const uniqueBy = (x, f) =>
   Object.values(x.reduce((a, b) => ((a[f(b)] = b), a), {}));
 
 const computeSum = (datas) => {
-  const lastDay = datas[0].dayId;
-  const lastDayDatas = datas.filter((d) => d.dayId === lastDay);
-  const uniqueLastDayDatas = uniqueBy(lastDayDatas, (d) => d.token);
-  return uniqueLastDayDatas
-    .map((d) => d.totalPrice)
-    .map((d) =>
-      Number.parseInt(round(Number.parseFloat(d) / 10 ** 18 / 10 ** 6, 0))
-    )
-    .reduce((a, b) => a + b, 0);
+  let unique = (a) => a.filter((item, i, ar) => ar.indexOf(item) === i);
+  let tokens = unique(datas.map((d) => d.token));
+  let sum = 0;
+  for (let token of tokens) {
+    const dataz = datas.filter((d) => d.token === token);
+    const lastDay = dataz[0].dayId;
+    const lastDayDatas = dataz.filter((d) => d.dayId === lastDay);
+    const uniqueLastDayDatas = uniqueBy(lastDayDatas, (d) => d.token);
+    const d = uniqueLastDayDatas[0].totalPrice;
+    const tokenSum = Number.parseInt(
+      round(Number.parseFloat(d) / 10 ** 18 / 10 ** 6, 0)
+    );
+    sum += tokenSum;
+  }
+  return sum;
 };
 
 const TotalPrice = (props) => {
@@ -52,11 +59,22 @@ const TotalPrice = (props) => {
     fn();
   }, [legacyTokens]);
 
-  if (loading || !data || !historicalData) {
+  const [historicalData2, setHistoricalData2] = useState();
+  useEffect(() => {
+    const fn = async () => {
+      const historicalData = await fetchCommonAll(tokens);
+      setHistoricalData2(historicalData);
+    };
+    fn();
+  }, [legacyTokens]);
+
+  if (loading || !data || !historicalData || !historicalData2) {
     return <>...</>;
   }
 
-  const graphPrice = computeSum(data.dayHistoricalDatas);
+  const dayHistoricalDatas = data ? data.dayHistoricalDatas : [];
+  const merged = mergeByDayID(historicalData2, dayHistoricalDatas);
+  const graphPrice = computeSum(merged);
   const legacyPrice = computeSum(historicalData);
   const totalPrice = graphPrice + legacyPrice;
 

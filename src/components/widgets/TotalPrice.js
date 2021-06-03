5eaba061a83a33cb736304ce7a65e917c633d71e
dvasin <dvasin@wavesplatform.com>
2021-06-03 10:20:01 +0300

    Fixed total sum

--- src/components/widgets/TotalPrice.js
+++ src/components/widgets/TotalPrice.js
@@ -2,20 +2,23 @@ import React, { useState, useEffect } from "react";
 import { useTokenDatas } from "./useTokenData";
 import { fetchCommonAll } from "./fetchCommon";
 import { round } from "./round";
-import { mergeByDayID } from "./merger";
 
 const uniqueBy = (x, f) =>
   Object.values(x.reduce((a, b) => ((a[f(b)] = b), a), {}));
 
 const computeSum = (datas) => {
   let unique = (a) => a.filter((item, i, ar) => ar.indexOf(item) === i);
-  let tokens = unique(datas.map((d) => d.token));
+  let tokens = unique(datas.map((d) => d.token.toLowerCase()));
   let sum = 0;
   for (let token of tokens) {
-    const dataz = datas.filter((d) => d.token === token);
+    const dataz = datas.filter(
+      (d) => d.token.toLowerCase() === token.toLowerCase()
+    );
     const lastDay = dataz[0].dayId;
     const lastDayDatas = dataz.filter((d) => d.dayId === lastDay);
-    const uniqueLastDayDatas = uniqueBy(lastDayDatas, (d) => d.token);
+    const uniqueLastDayDatas = uniqueBy(lastDayDatas, (d) =>
+      d.token.toLowerCase()
+    );
     const d = uniqueLastDayDatas[0].totalPrice;
     const tokenSum = Number.parseInt(
       round(Number.parseFloat(d) / 10 ** 18 / 10 ** 6, 0)

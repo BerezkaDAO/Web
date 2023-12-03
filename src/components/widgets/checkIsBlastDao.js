ef9443e246cd8ac5db7cdd3bb3d3f84b93225dae
Вячеслав Дутка <vsdutka@gmail.com>
2023-12-03 19:18:44 +0300

    Add dev blast dao id

--- src/components/widgets/checkIsBlastDao.js
+++ src/components/widgets/checkIsBlastDao.js
@@ -2,7 +2,8 @@ export const checkIsBlastDao = (daoIdOrDaoAdress) => {
   if (daoIdOrDaoAdress) {
     return (
       daoIdOrDaoAdress === "9f9601eb-2f7c-689c-24f0-77ef37c60853" ||
-      daoIdOrDaoAdress === "0x50987cf58b7351867952912cf87c75ab2a8a60a4"
+      daoIdOrDaoAdress === "0x50987cf58b7351867952912cf87c75ab2a8a60a4" ||
+      daoIdOrDaoAdress === "61371f22-3854-e8b7-de97-6fe362301019"
     );
   }
 };
